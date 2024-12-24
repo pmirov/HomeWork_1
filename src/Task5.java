3f4e80ab7467c0bd72597c77da1ef684513a40dc
Pavel Mirovshchikov <pmirov@gmail.com>
2024-12-24 22:33:44 +0700

    Исправил Switch, добавил вывод текста вначале

--- src/Task5.java
+++ src/Task5.java
@@ -3,25 +3,39 @@ import java.util.Scanner;
 public class Task5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String text = scanner.nextLine();
-        //System.out.println(text);
-        switch (text)
+        System.out.println("Введите число 1 до 12:");
+        while (true)
         {
-            case "1":
-                System.out.println("Winter");
-                break;
-            case "2":
-                System.out.println("Spring");
-                break;
-            case "3":
-                System.out.println("Summer");
-                break;
-            case "4":
-                System.out.println("Autumn");
-                break;
-            default:
-                System.out.println("Ошибка!");
-                break;
+            String text = scanner.nextLine();
+            switch (text)
+            {
+                case "1":
+                case "2":
+                case "12":
+                    System.out.println("Winter");
+                    break;
+                case "3":
+                case "4":
+                case "5":
+                    System.out.println("Spring");
+                    break;
+                case "6":
+                case "7":
+                case "8":
+                    System.out.println("Summer");
+                    break;
+                case "9":
+                case "10":
+                case "11":
+                    System.out.println("Autumn");
+                    break;
+                default:
+                    System.out.println("Ошибка! Нужно ввести число 1 до 12");
+                    break;
+            }
+
+
+
         }
 
 
