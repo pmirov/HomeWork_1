c4a01ce430343aa5b946982381a00512fadd5b3c
Pavel Mirovshchikov <pmirov@gmail.com>
2024-12-24 22:14:30 +0700

    добавил ввод текста

--- src/Task5.java
+++ src/Task5.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Task5 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        //System.out.println(text);
+
     }
 }
