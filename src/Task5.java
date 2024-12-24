d8891bf140a5a5366cf35b423f672a9d83eddf19
Pavel Mirovshchikov <pmirov@gmail.com>
2024-12-24 22:19:27 +0700

    Добавил Switch case

--- src/Task5.java
+++ src/Task5.java
@@ -5,6 +5,25 @@ public class Task5 {
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
         //System.out.println(text);
+        switch (text)
+        {
+            case "1":
+                System.out.println("Winter");
+                break;
+            case "2":
+                System.out.println("Spring");
+                break;
+            case "3":
+                System.out.println("Summer");
+                break;
+            case "4":
+                System.out.println("Autumn");
+                break;
+            default:
+                System.out.println("Ошибка!");
+                break;
+        }
+
 
     }
 }
