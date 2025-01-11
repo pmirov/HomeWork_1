32d4eecafa07d0eaf6ae1d1987f3bfae3f78a336
Pavel Mirovshchikov <pmirov@gmail.com>
2025-01-11 13:01:14 +0700

    добавил проверку на ввод целых чисел

--- src/Task6.java
+++ src/Task6.java
@@ -4,7 +4,15 @@ public class Task6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите количество метров:");
-        int length = Integer.parseInt(scanner.nextLine());
+        int length;
+        while (true) {
+            try {
+                length = Integer.parseInt(scanner.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Ошибка! Введите целое число.");
+            }
+        }
         System.out.println("Выберите единицу измерения: \n1 - метр \n2 - дюйм \n3 - ярд ");
         String valueLength = scanner.nextLine();
         switch (valueLength)
@@ -13,12 +21,11 @@ public class Task6 {
                 System.out.println("Длина в метрах: " + length + "м");
                 break;
             case "2":
-                System.out.println("Длина в дюймах: " + (length * 39.37) + "д");
+                System.out.println("Длина в дюймах: " + length * 39.37 + "д");
                 break;
             case "3":
-                System.out.println("Длина в ярдах: " + length + "yd");
+                System.out.println("Длина в ярдах: " + length * 1.09 + "yd");
                 break;
-
             default:
                 System.out.println("Ошибка! Нужно ввести число 1 до 3");
                 break;
