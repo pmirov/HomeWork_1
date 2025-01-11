5c8c355698552481fb4a2a9cf96248103739d500
Pavel Mirovshchikov <pmirov@gmail.com>
2025-01-11 13:37:19 +0700

    добавил проверку ввода чисел

--- src/Task7.java
+++ src/Task7.java
@@ -4,10 +4,19 @@ public class Task7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите два числа через пробел");
-        String input = scanner.nextLine();
-        String[] numbers = input.split(" ");
-        int num1 = Integer.parseInt(numbers[0]);
-        int num2 = Integer.parseInt(numbers[1]);
+        int num1;
+        int num2;
+        while (true) {
+            try {
+                String input = scanner.nextLine();
+                String[] numbers = input.split(" ");
+                num1 = Integer.parseInt(numbers[0]);
+                num2 = Integer.parseInt(numbers[1]);                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Ошибка! Введите целое число.");
+            }
+        }
+
         if (num1 > num2)
         {
             int tmp;
