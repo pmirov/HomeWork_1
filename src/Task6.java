db2e822f4b3eca65b4d0835ef280091aa98aa704
Pavel Mirovshchikov <pmirov@gmail.com>
2025-01-11 12:53:49 +0700

    добавил преобразование в int вводимого числа

--- src/Task6.java
+++ src/Task6.java
@@ -4,7 +4,24 @@ public class Task6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите количество метров:");
-        String value = scanner.nextLine();
+        int length = Integer.parseInt(scanner.nextLine());
+        System.out.println("Выберите единицу измерения: \n1 - метр \n2 - дюйм \n3 - ярд ");
+        String valueLength = scanner.nextLine();
+        switch (valueLength)
+        {
+            case "1":
+                System.out.println("Длина в метрах: " + length + "м");
+                break;
+            case "2":
+                System.out.println("Длина в дюймах: " + (length * 39.37) + "д");
+                break;
+            case "3":
+                System.out.println("Длина в ярдах: " + length + "yd");
+                break;
 
+            default:
+                System.out.println("Ошибка! Нужно ввести число 1 до 3");
+                break;
+        }
     }
 }
