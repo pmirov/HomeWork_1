15834b7245705b92ba8c9f56f33be32ffa4ccaab
Pavel Mirovshchikov <pmirov@gmail.com>
2025-01-11 13:33:02 +0700

    сделал нормализацию границ диапазона

--- src/Task7.java
+++ src/Task7.java
@@ -8,6 +8,14 @@ public class Task7 {
         String[] numbers = input.split(" ");
         int num1 = Integer.parseInt(numbers[0]);
         int num2 = Integer.parseInt(numbers[1]);
+        if (num1 > num2)
+        {
+            int tmp;
+            tmp = num2;
+            num2 = num1;
+            num1 = tmp;
+
+        }
         for (int i = num1; i <= num2; i++) {
             if (i%2 != 0) {
                 System.out.println(i + " ");
