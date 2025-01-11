2c771d5d06e5c543b18e8eeb7482c5c2ef19aa1a
Pavel Mirovshchikov <pmirov@gmail.com>
2025-01-11 12:41:11 +0700

    добавил ввод числа

--- src/Task6.java
+++ src/Task6.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Task6 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество метров:");
+        String value = scanner.nextLine();
+
     }
 }
