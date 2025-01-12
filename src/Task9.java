5fe98f8c3d7f6dee00fbb3c6aec6ba9e2437ab9c
Pavel Mirovshchikov <pmirov@gmail.com>
2025-01-12 16:59:48 +0700

    создал массив

--- src/Task9.java
+++ src/Task9.java
@@ -1,5 +1,9 @@
+import java.util.Random;
+
 public class Task9 {
     public static void main(String[] args) {
+        int[] array = new int[10];
+        Random random = new Random();
         
     }
 }
