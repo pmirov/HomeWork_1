070364e71860aff13c184b7f7e0009fe9c954e4f
Pavel Mirovshchikov <pmirov@gmail.com>
2025-01-12 18:25:16 +0700

    сделал подсчет переменных

--- src/Task10.java
+++ src/Task10.java
@@ -8,5 +8,33 @@ public class Task10 {
             array[i] = random.nextInt(-10,11);
             System.out.print(array[i] + " ");
         }
+        int[] evenNumbers = new int[]{};
+        int[] oddNumbers;
+        int[] negativeNumbers;
+        int[] positiveNumbers;
+        int numOdds = 0;
+        int numEvens = 0;
+        int numNegative = 0;
+        int numPositive = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0)
+            {
+                numPositive++;
+            }
+            if (array[i] < 0)
+            {
+                numNegative++;
+            }
+            if (array[i]%2 == 0)
+            {
+                numEvens++;
+            }
+            if (array[i]%2 != 0)
+            {
+                numOdds++;
+            }
+
+
+        }
     }
 }
