27a6d03344c7ad2c63d768393815abadd8f2d409
Pavel Mirovshchikov <pmirov@gmail.com>
2025-01-12 17:30:30 +0700

    добавил определение и вывод макс. и мин. элементов

--- src/Task9.java
+++ src/Task9.java
@@ -5,12 +5,14 @@ public class Task9 {
         int[] array = new int[15];
         Random random = new Random();
         for (int i = 0; i < array.length; i++) {
-            array[i] = random.nextInt(-10,10);
+            array[i] = random.nextInt(-10,11);
             System.out.print(array[i] + " ");
         }
         int numPos = 0;
         int numNeg = 0;
         int numNull = 0;
+        int min = array[0];
+        int max = array[0];
         for (int i = 0; i < array.length; i++) {
             if (array[i] > 0)
             {
@@ -24,10 +26,21 @@ public class Task9 {
             {
                 numNull++;
             }
+            if (array[i] < min)
+            {
+                min = array[i];
+            }
+            if (array[i] > max)
+            {
+                max = array[i];
+            }
         }
         System.out.println("\nКоличество отрицательных элементов: " + numNeg +
                            "\nКоличество положительных элеменов: " + numPos +
-                           "\nКоличество нулей: " + numNull
+                           "\nКоличество нулей: " + numNull +
+                           "\nМинимальный элемент: " + min +
+                           "\nМаксимальный элемент: " + max
+
         );
     }
 }
