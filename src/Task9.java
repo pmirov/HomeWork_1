ecb1858cce24de28b0e49826110c304c8d157edc
Pavel Mirovshchikov <pmirov@gmail.com>
2025-01-12 17:05:07 +0700

    добавил наполнение массива случайными числами и вывод готового массива

--- src/Task9.java
+++ src/Task9.java
@@ -2,8 +2,11 @@ import java.util.Random;
 
 public class Task9 {
     public static void main(String[] args) {
-        int[] array = new int[10];
+        int[] array = new int[15];
         Random random = new Random();
-        
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(-10,10);
+            System.out.print(array[i] + " ");
+        }
     }
 }
