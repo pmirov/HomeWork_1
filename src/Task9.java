d3f1b038fb4e73fde5fc05477ff411bae827af1c
Pavel Mirovshchikov <pmirov@gmail.com>
2025-01-12 17:24:08 +0700

    добавил подсчет количества отрицательных, положительных элементов, нулей

--- src/Task9.java
+++ src/Task9.java
@@ -8,5 +8,26 @@ public class Task9 {
             array[i] = random.nextInt(-10,10);
             System.out.print(array[i] + " ");
         }
+        int numPos = 0;
+        int numNeg = 0;
+        int numNull = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0)
+            {
+                numPos++;
+            }
+            if (array[i] < 0)
+            {
+                numNeg++;
+            }
+            if (array[i] == 0)
+            {
+                numNull++;
+            }
+        }
+        System.out.println("\nКоличество отрицательных элементов: " + numNeg +
+                           "\nКоличество положительных элеменов: " + numPos +
+                           "\nКоличество нулей: " + numNull
+        );
     }
 }
