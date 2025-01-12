5b0b8e8f6ab35913bd5c04d8f9d1df8d56a2ffcf
Pavel Mirovshchikov <pmirov@gmail.com>
2025-01-12 16:38:04 +0700

    добавил ввод 2 чисел с проверкой

--- src/Task8.java
+++ src/Task8.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Task8 {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите два целых числа через пробел");
+        int num1;
+        int num2;
+        while (true) {
+            try {
+                String input = scanner.nextLine();
+                String[] numbers = input.split(" ");
+                num1 = Integer.parseInt(numbers[0]);
+                num2 = Integer.parseInt(numbers[1]);                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Ошибка! Введите целое число.");
+            }
+        }
     }
 }
