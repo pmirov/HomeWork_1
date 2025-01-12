0f9bc6127c83f2b2732ec52edcd4b0b17cd18e56
Pavel Mirovshchikov <pmirov@gmail.com>
2025-01-12 21:45:37 +0700

    добавил метот создания линии

--- src/Task11.java
+++ src/Task11.java
@@ -1,5 +1,12 @@
 public class Task11 {
     public static void main(String[] args) {
-        
+
+       
+
+        }
+    public void drawLine(char symbol, int length, boolean isHorizontal) {
+        for (int i = 0; i < length; i++) {
+            System.out.println(symbol);
+        }
     }
 }
