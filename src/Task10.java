d4baf11f17dc7ace5fd2f7a5fded05f8b0fcf87d
Pavel Mirovshchikov <pmirov@gmail.com>
2025-01-12 18:08:49 +0700

    добавил массив и заполнение случайными числами

--- src/Task10.java
+++ src/Task10.java
@@ -1,5 +1,12 @@
+import java.util.Random;
+
 public class Task10 {
     public static void main(String[] args) {
-        
+        int[] array = new int[15];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(-10,11);
+            System.out.print(array[i] + " ");
+        }
     }
 }
