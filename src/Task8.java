ff5f55f069fca13f9210cd892b9f561779e30d0b
Pavel Mirovshchikov <pmirov@gmail.com>
2025-01-12 16:50:00 +0700

    добавил расчет и вывод таблицы умножения

--- src/Task8.java
+++ src/Task8.java
@@ -17,5 +17,12 @@ public class Task8 {
                 System.out.println("Ошибка! Введите целое число.");
             }
         }
+        for (int i = num1; i <= num2; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print(" "+ i + " * " + j + " = " + i*j);
+            }
+            System.out.println();
+        }
+
     }
 }
